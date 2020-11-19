227b1995f538f6bf9919bc75baeb3f76dbeb9ab6
Rocco <rocco.chiarella.rc@gmail,com>
2020-11-19 16:03:14 +0100

    Programma completo

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+Vue.config.devtools = true;
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            nome: '',
+            cognome: ''
+        },
+        methods: {
+
+        },
+        computed : {
+
+        }
+    }
+);
